6e9de6d0295e72afe11ab0dd0d2ca9fa8d096693
Simran Das <122141330+simrandas09@users.noreply.github.com>
2025-09-20 04:11:18 +0530

    Time: 96 ms (71.9%), Space: 23.5 MB (70.39%) - LeetHub

---
+++ 3797-design-spreadsheet/3797-design-spreadsheet.py
@@ -0,0 +1,23 @@
+class Spreadsheet:
+    def __init__(self, rows: int):
+        self.dict = defaultdict(int)
+    def setCell(self, cell: str, value: int) -> None:
+        self.dict[cell] = value
+    def resetCell(self, cell: str) -> None:
+        self.dict[cell] = 0
+    def getValue(self, formula: str) -> int:
+        ops = formula.split('=')[1].split('+')
+        res = 0
+        for op in ops:
+            if op in self.dict:
+                res += self.dict[op]
+            elif op.isdigit():
+                res += int(op)
+        return res
+
+
+# Your Spreadsheet object will be instantiated and called as such:
+# obj = Spreadsheet(rows)
+# obj.setCell(cell,value)
+# obj.resetCell(cell)
+# param_3 = obj.getValue(formula)
